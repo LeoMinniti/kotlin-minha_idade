ce0b9aad2535f46a8b82035525748bab47542123
leonardo.minniti <leonsenai@gmail.com>
2024-10-31 22:17:08 -0300

    Finalizando a aula de 31/10/2024

--- app/src/main/java/br/com/fiap/minhaidade/MainActivity.kt
+++ app/src/main/java/br/com/fiap/minhaidade/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -15,6 +17,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -39,9 +43,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 fun CounterScreen() {
+
+    var idade = remember {
+        mutableStateOf(0);
+    }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
@@ -56,21 +65,25 @@ fun CounterScreen() {
         Text(
             text = "Aperte os botões para informar a  sua idade?",
             fontSize = 12.sp
-            )
+        )
         Text(
-            text = "21",
-            fontSize = 48.sp, 
+            text = "${idade.value}",
+            fontSize = 48.sp,
             fontWeight = FontWeight.Bold
-            )
-    //quando abro uma linha -> organiza na horizontal
+        )
+        //quando abro uma linha -> organiza na horizontal
         Row() {
-            Button(onClick = {},
-             modifier=Modifier.size(84.dp),
-             shape = RoundedCornerShape(8.dp),
-             colors = ButtonDefaults.buttonColors(Color(0xFFAD1f4e)))
+            Button(
+                onClick = { idade.value-- },
+                modifier=Modifier.size(84.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFFAD1f4e)))
             { Text(text = "-", fontSize = 40.sp)}
 
-            Button(onClick = {},
+            Spacer(modifier = Modifier.width(32.dp));
+
+            Button(
+                onClick = { idade.value++ },
                 modifier=Modifier.size(84.dp),
                 shape = RoundedCornerShape(8.dp),
                 colors = ButtonDefaults.buttonColors(Color(0xFFAD1f4e)))
